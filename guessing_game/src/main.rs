ea50d984e92b1369e84dfb89188bcbbe15c4a915
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-24 12:33:24 +0900

    エラーになった際の処理を記述。

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,30 +8,23 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     //範囲は下限値を含み、上限値を含まないため
 
-    println!("The secret number is : {}", secret_number);
+    // println!("The secret number is : {}", secret_number);
 
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
-        //let foo = 5; // immutable
-        //let mut bar = 5; // mutable
-
-        // ::new行にある::という記法は、newがString型の関連関数であることを表しています。 関連関数とは、String型の特定のオブジェクトよりも型(この場合はString)に対して 実装された関数のことであり、静的(スタティック)メソッドと呼ばれる言語もあります。
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
         // 仮に、プログラムの冒頭でuse std::ioとしていなければ、この関数呼び出しは、std::io::stdinと記述していたでしょう。
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
-
-        /* 別の例(PythonのFormatに似てる)
-        let x = 5;
-        let y = 10;
-        println!("x = {} and y = {}", x, y); */
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
